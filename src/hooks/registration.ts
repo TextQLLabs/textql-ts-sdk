2d7201dc23aa058084902bc3a512ef81ad609fb9
Rodney Shen <rodneyshenn@gmail.com>
2026-07-22 17:30:05 -0400

    pre hook

--- src/hooks/registration.ts
+++ src/hooks/registration.ts
@@ -1,4 +1,4 @@
-import { Hooks } from "./types.js";
+import { BeforeRequestContext, BeforeRequestHook, Hooks } from "./types.js";
 
 /*
  * This file is only ever generated once on the first generation and then is free to be modified.
@@ -6,9 +6,22 @@ import { Hooks } from "./types.js";
  * in this file or in separate files in the hooks folder.
  */
 
-// @ts-expect-error remove this line when you add your first hook and hooks is used
+const RPC_PREFIX = "/rpc/public";
+
+class RPCPublicPrefixHook implements BeforeRequestHook {
+  beforeRequest(_hookCtx: BeforeRequestContext, request: Request): Request {
+    const url = new URL(request.url);
+    if (url.pathname.includes(RPC_PREFIX) || !url.pathname.startsWith("/textql.")) {
+      return request;
+    }
+    url.pathname = `${RPC_PREFIX}${url.pathname}`;
+    return new Request(url, request);
+  }
+}
+
 export function initHooks(hooks: Hooks) {
   // Add hooks by calling hooks.register{ClientInit/BeforeCreateRequest/BeforeRequest/AfterSuccess/AfterError}Hook
   // with an instance of a hook that implements that specific Hook interface
   // Hooks are registered per SDK instance, and are valid for the lifetime of the SDK instance
+  hooks.registerBeforeRequestHook(new RPCPublicPrefixHook());
 }
